1df28bbb2a5a2e3b8229290192235a2971ee72f3
Felipe Guimarães <feliphe133bolado@gmail.com>
2025-06-14 12:43:01 -0300

    Adicionando classes Contador e ParametrosInvalidosException

---
+++ src/main/java/Account/exception/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package Account.exception;
+
+public class ParametrosInvalidosException extends Exception {
+    
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+    
+    public ParametrosInvalidosException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
+}
